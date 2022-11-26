458d5a389fdf2c9c23ec24d4b62ea37d4bf1231f
tangzhiqinag <t1339803837@163.com>
2022-11-26 11:14:26 +0800

    feat:add ssl test case

--- test-server/main.go
+++ test-server/main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"bufio"
+	"crypto/tls"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 )
 
 var (
 	listenAddr = flag.String("http", ":9090", "http listen address")
+	useHttps   = flag.Bool("useHttps", false, "use https")
+	useSsl     = flag.Bool("useHttps", false, "use https")
 )
 
 func main() {
 	flag.Parse()
+
+	if *useSsl {
+		cert, err := tls.LoadX509KeyPair("server.pem", "server.key")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		config := &tls.Config{Certificates: []tls.Certificate{cert}}
+		ln, err := tls.Listen("tcp", ":443", config)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer ln.Close()
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			go handleConn(conn)
+		}
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		hBytes, _ := json.Marshal(r.Header)
@@ -35,4 +64,22 @@ func main() {
 
 	log.Printf("start success! listen address is %+v", *listenAddr)
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	r := bufio.NewReader(conn)
+	for {
+		msg, err := r.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		println(msg)
+		n, err := conn.Write([]byte("world\n"))
+		if err != nil {
+			log.Println(n, err)
+			return
+		}
+	}
 }
