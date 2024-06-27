d1069da7d772f5401efb0254058fe5d6210539e0
blackcloud <t1339803847@163.com>
2024-06-27 09:21:29 +0800

    refactor: update

--- common/utils/net/check.go
+++ common/utils/net/check.go
@@ -9,18 +9,14 @@ import (
 
 // ConnectivityTest 检查地址的连接性
 func ConnectivityTest(ipPorts string) (err error) {
-	netRes, netErr := net.DialTimeout("tcp", ipPorts, time.Second*3)
-	if netErr != nil {
-		return netErr
+	netRes, err := net.DialTimeout("tcp", ipPorts, time.Second*3)
+	if err != nil {
+		return err
 	}
 	if netRes == nil {
 		return errors.New(fmt.Sprintf("the %s is disabled.", ipPorts))
 	}
 
-	if netRes != nil {
-		_ = netRes.Close()
-		return nil
-	}
-
+	_ = netRes.Close()
 	return
 }
