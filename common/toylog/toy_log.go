c4c3c67b100d90cb2ff921d86ffc579cc9455fec
blackcloud <t1339803847@163.com>
2024-07-13 11:27:38 +0800

    feat: update

--- common/toylog/toy_log.go
+++ common/toylog/toy_log.go
@@ -13,7 +13,6 @@ import (
 	"time"
 
 	timeUtil "github.com/leihenshang/http-little-toy/common/utils/datetime"
-	fileUtil "github.com/leihenshang/http-little-toy/common/utils/file"
 )
 
 // ToyLog a log object
@@ -32,14 +31,14 @@ func NewMyLog() *ToyLog {
 }
 
 func logInit(LogDir string) (f *os.File, err error) {
-	if !fileUtil.IsFileExisted(LogDir) {
+	if _, err = os.Stat(LogDir); os.IsNotExist(err) {
 		if err = os.MkdirAll(LogDir, os.ModePerm); err != nil {
 			err = errors.New(fmt.Sprintf("an error occurred while make directory.err:%v \n", err))
 			return
 		}
 	}
 
-	logName := fmt.Sprintf("httpLittleToy-%s.log", time.Now().Format(timeUtil.DateTimeFormatCustom))
+	logName := fmt.Sprintf("http-little-toy-%s.log", time.Now().Format(timeUtil.DateTimeFormatCustom))
 	logPath := path.Join(LogDir, logName)
 	if f, err = os.OpenFile(logPath, os.O_RDWR|os.O_CREATE, fs.ModePerm); err != nil {
 		err = errors.New(fmt.Sprintf("an error occurred while create log file.err:%v \n", err))
@@ -50,12 +49,14 @@ func logInit(LogDir string) (f *os.File, err error) {
 
 // Start logging
 func (m *ToyLog) Start(ctx context.Context, logDir string) (err error) {
-	logFile, logErr := logInit(logDir)
-	if logErr != nil {
-		return logErr
+	logFile, err := logInit(logDir)
+	if err != nil {
+		return err
 	}
 
 	go func() {
+		defer logFile.Close()
+
 	LOOP:
 		for {
 			select {
@@ -66,7 +67,7 @@ func (m *ToyLog) Start(ctx context.Context, logDir string) (err error) {
 				buf.Write(l)
 				buf.WriteString("\n")
 				if _, err = logFile.Write(buf.Bytes()); err != nil {
-					log.Printf("[Start] write log err:%v\n", err)
+					log.Printf("write toy log err:%v\n", err)
 				}
 			case <-ctx.Done():
 				break LOOP
@@ -74,7 +75,6 @@ func (m *ToyLog) Start(ctx context.Context, logDir string) (err error) {
 
 		}
 
-		logFile.Close()
 	}()
 
 	return
