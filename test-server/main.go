b90a5400ac4faacb580a5479cbd0f256db4f5bf5
tangzq <tangzq>
2025-07-02 11:49:53 +0800

    fix: 修复请求处理中的错误处理逻辑并优化代码结构

--- test-server/main.go
+++ test-server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -13,33 +12,35 @@ var (
 	listenAddr = flag.String("http", ":9090", "http listen address")
 	certFile   = flag.String("certFile", "", "cert file")
 	keyFile    = flag.String("keyFile", "", "cert key")
+	http2      = flag.Bool("h2", false, "use http2 protocol")
 )
 
 func main() {
 	flag.Parse()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		hBytes, _ := json.Marshal(r.Header)
-		body := r.Body
-		if body != nil {
-			defer body.Close()
+		hBytes, err := json.Marshal(r.Header)
+		if err != nil {
+			log.Printf("marshal header error: %v", err)
+			http.Error(w, "Failed to marshal header", http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+		bodyBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("read body error: %v", err)
+			http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+			return
 		}
 
-		bodyBytes, _ := io.ReadAll(body)
-
-		w.Write([]byte("welcome to shop! \n"))
-		fmt.Println("header:" + string(hBytes) + "\n")
+		w.Write([]byte("welcome to toy testing! \n"))
 		w.Write([]byte("header:" + string(hBytes) + "\n"))
-		fmt.Println("body:" + string(bodyBytes) + "\n")
 		w.Write([]byte("body:" + string(bodyBytes) + "\n"))
 
 	})
-	log.Printf("start success! listen address is %+v", *listenAddr)
-	if *certFile != "" {
-		if *keyFile == "" {
-			log.Fatal("cert file can not be empty")
-		}
 
+	log.Printf("start test server on: %+v", *listenAddr)
+	if *http2 {
 		log.Fatal(http.ListenAndServeTLS(*listenAddr, *certFile, *keyFile, nil))
 	} else {
 		log.Fatal(http.ListenAndServe(*listenAddr, nil))
